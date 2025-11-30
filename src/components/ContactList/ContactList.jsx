36f8d128e3b01b78ad819548d47cf0bfacb17001
Borys Ziukov <ziukoff1985@gmail.com>
2025-11-30 21:27:14 +0200

    Refactoring ContactList from class to function

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,35 +3,39 @@ import ContactItem from '../ContactItem/ContactItem';
 
 import styles from './ContactList.module.css';
 
-export class ContactList extends Component {
-    render() {
-        const { contacts, onAddNewContact } = this.props;
+function ContactList(props) {
+    const {
+        contactForEdit,
+        contacts,
+        onAddNewContact,
+        onDeleteContact,
+        onEditContact,
+    } = props;
 
-        return (
-            <div className={styles.contactListWrapper}>
-                <ul className={styles.contactList}>
-                    {contacts.length === 0
-                        ? 'No contacts yet'
-                        : contacts.map((contact) => (
-                              <ContactItem
-                                  key={contact.id}
-                                  contact={contact}
-                                  onDeleteContact={this.props.onDeleteContact}
-                                  onEditContact={this.props.onEditContact}
-                                  contactForEdit={this.props.contactForEdit}
-                              />
-                          ))}
-                </ul>
-                <button
-                    className={styles.addButton}
-                    type='button'
-                    onClick={onAddNewContact}
-                >
-                    New
-                </button>
-            </div>
-        );
-    }
+    return (
+        <div className={styles.contactListWrapper}>
+            <ul className={styles.contactList}>
+                {contacts.length === 0
+                    ? 'No contacts yet'
+                    : contacts.map((contact) => (
+                          <ContactItem
+                              key={contact.id}
+                              contact={contact}
+                              onDeleteContact={onDeleteContact}
+                              onEditContact={onEditContact}
+                              contactForEdit={contactForEdit}
+                          />
+                      ))}
+            </ul>
+            <button
+                className={styles.addButton}
+                type='button'
+                onClick={onAddNewContact}
+            >
+                New
+            </button>
+        </div>
+    );
 }
 
 export default ContactList;
