76c64c47ee064ba08eec07255622ec06b981a6d7
Borys Ziukov <ziukoff1985@gmail.com>
2025-11-30 21:35:18 +0200

    Refactoring ContactItem from class to function

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,37 +1,35 @@
 import { Component } from 'react';
 import styles from './ContactItem.module.css';
 
-export class ContactItem extends Component {
-    onContactDelete = () => {
-        this.props.onDeleteContact(this.props.contact.id);
+function ContactItem(props) {
+    const { contact, contactForEdit, onDeleteContact, onEditContact } = props;
+    const onContactDelete = () => {
+        onDeleteContact(contact.id);
     };
 
-    onContactEdit = () => {
-        this.props.onEditContact(this.props.contact);
+    const onContactEdit = () => {
+        onEditContact(contact);
     };
 
-    render() {
-        const { contact, contactForEdit } = this.props;
-        return (
-            <li
-                className={`${styles.contactItem} ${
-                    contactForEdit?.id === contact.id && styles.updating
-                }`}
-                onDoubleClick={this.onContactEdit}
+    return (
+        <li
+            className={`${styles.contactItem} ${
+                contactForEdit?.id === contact.id && styles.updating
+            }`}
+            onDoubleClick={onContactEdit}
+        >
+            <div className={styles.contactName}>
+                {contact.firstName} {contact.lastName}
+            </div>
+            <button
+                className={styles.deleteButton}
+                type='button'
+                onClick={onContactDelete}
             >
-                <div className={styles.contactName}>
-                    {contact.firstName} {contact.lastName}
-                </div>
-                <button
-                    className={styles.deleteButton}
-                    type='button'
-                    onClick={this.onContactDelete}
-                >
-                    ❌
-                </button>
-            </li>
-        );
-    }
+                ❌
+            </button>
+        </li>
+    );
 }
 
 export default ContactItem;
