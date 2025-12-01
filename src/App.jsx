e3842df1961d30150227ab7166400f761460a462
Borys Ziukov <ziukoff1985@gmail.com>
2025-12-01 13:29:51 +0200

    Refactoring App from class to function

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 
 import styles from './App.module.css';
 import ContactList from './components/ContactList/ContactList';
 import ContactForm from './components/ContactForm/ContactForm';
 
-export class App extends Component {
-    state = {
-        contacts: [],
-        contactForEdit: this.createEmptyContact(),
-    };
+function App() {
+    const [contacts, setContacts] = useState(getContactsFromLocalStorage());
+    const [contactForEdit, setContactForEdit] = useState(createEmptyContact());
 
-    createEmptyContact() {
+    useEffect(() => {
+        localStorage.setItem('contacts', JSON.stringify(contacts));
+    }, [contacts]);
+
+    function getContactsFromLocalStorage() {
+        const savedData = JSON.parse(localStorage.getItem('contacts'));
+        return savedData ?? [];
+    }
+
+    function createEmptyContact() {
         return {
             id: null,
             firstName: '',
@@ -21,101 +28,66 @@ export class App extends Component {
         };
     }
 
-    componentDidMount() {
-        const contacts = JSON.parse(localStorage.getItem('contacts'));
-        if (!contacts) {
-            this.setState({ contacts: [] });
-        } else {
-            this.setState({ contacts: contacts });
-        }
-    }
-
-    deleteContact = (contactId) => {
-        this.setState((prevState) => {
-            const contacts = prevState.contacts.filter(
-                (contact) => contact.id !== contactId
-            );
-            const isContactUpdating = prevState.contactForEdit.id === contactId;
-            this.saveContactsToLocalStorage(contacts);
-            return {
-                contacts: contacts,
-                contactForEdit: isContactUpdating
-                    ? this.createEmptyContact()
-                    : prevState.contactForEdit,
-            };
-        });
-    };
-
-    saveContact = (contact) => {
-        if (!contact.id) {
-            this.createContact(contact);
-        } else {
-            this.updateContact(contact);
-        }
-    };
-
-    addNewContact = () => {
-        this.setState({
-            contactForEdit: this.createEmptyContact(),
-        });
-    };
-
-    editContact = (contact) => {
-        this.setState({
-            contactForEdit: { ...contact },
-        });
-    };
-
-    createContact = (contact) => {
-        contact.id = nanoid();
-        this.setState((prevState) => {
-            const contacts = [...prevState.contacts, contact];
-            this.saveContactsToLocalStorage(contacts);
-            return {
-                contacts: contacts,
-                contactForEdit: this.createEmptyContact(),
-            };
-        });
-    };
-
-    updateContact = (contact) => {
-        this.setState((prevState) => {
-            const contacts = prevState.contacts.map((item) =>
-                item.id === contact.id ? contact : item
-            );
-            this.saveContactsToLocalStorage(contacts);
-            return {
-                contacts: contacts,
-                contactForEdit: contact,
-            };
-        });
-    };
-
-    saveContactsToLocalStorage = (contacts) => {
-        localStorage.setItem('contacts', JSON.stringify(contacts));
-    };
-
-    render() {
-        return (
-            <>
-                <h1>Contact List</h1>
-                <div className={styles.container}>
-                    <ContactList
-                        contacts={this.state.contacts}
-                        onDeleteContact={this.deleteContact}
-                        onAddNewContact={this.addNewContact}
-                        onEditContact={this.editContact}
-                        contactForEdit={this.state.contactForEdit}
-                    />
-                    <ContactForm
-                        contactForEdit={this.state.contactForEdit}
-                        onSubmit={this.saveContact}
-                        onDeleteContact={this.deleteContact}
-                    />
-                </div>
-            </>
+    function deleteContact(contactId) {
+        const newContacts = contacts.filter(
+            (contact) => contact.id !== contactId
+        );
+        const isContactNowUpdating = contactForEdit.id === contactId;
+        setContacts(newContacts);
+        setContactForEdit(
+            isContactNowUpdating ? createEmptyContact() : contactForEdit
         );
     }
+
+    function saveContact(contact) {
+        if (!contact.id) {
+            createContact(contact);
+        } else {
+            updateContact(contact);
+        }
+    }
+
+    function addNewContact() {
+        setContactForEdit(createEmptyContact());
+    }
+
+    function editContact(contact) {
+        setContactForEdit({ ...contact });
+    }
+
+    function createContact(contact) {
+        contact.id = nanoid();
+        setContacts((prevState) => [...prevState, contact]);
+        setContactForEdit(createEmptyContact());
+        console.log(contacts);
+    }
+
+    function updateContact(contact) {
+        setContacts((prevState) =>
+            prevState.map((item) => (item.id === contact.id ? contact : item))
+        );
+        setContactForEdit({ ...contact });
+    }
+
+    return (
+        <>
+            <h1>Contact List</h1>
+            <div className={styles.container}>
+                <ContactList
+                    contacts={contacts}
+                    onDeleteContact={deleteContact}
+                    onAddNewContact={addNewContact}
+                    onEditContact={editContact}
+                    contactForEdit={contactForEdit}
+                />
+                <ContactForm
+                    contactForEdit={contactForEdit}
+                    onSubmit={saveContact}
+                    onDeleteContact={deleteContact}
+                />
+            </div>
+        </>
+    );
 }
 
 export default App;
